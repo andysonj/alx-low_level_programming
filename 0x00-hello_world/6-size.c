f1940136b55e87dc8090d6ba8b878ff6e1360f5c
andysonj <andymfuko@gmail.com>
2023-02-19 14:42:37 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,10 @@ float floatType;
 double doubleType;
 char charType;
 
-printf("Size of int: %zu bytes(s)\n", sizeof(intType));
-printf("Size of float: %zu bytes(s)\n", sizeof(floatType));
-printf("Size of double: %zu bytes(s)\n", sizeof(doubleType));
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long));
+printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
 return (0);
 }
