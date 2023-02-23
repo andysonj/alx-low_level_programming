aca37312db39c4a5838534894a2d15e20584ad84
andysonj <andymfuko@gmail.com>
2023-02-22 19:00:24 -0800

    new commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,15 +2,16 @@
 /**
  *main -  print alphabets except e and q
  *
- * Return:Alway 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
-			putchar(letter);;
+			putchar(letter);
 	}
 	putchar('\n');
 	return (0);
