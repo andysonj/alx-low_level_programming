42538b44617098adbdf4f731f7dc78b7f14c4898
andysonj <andymfuko@gmail.com>
2023-03-10 05:43:06 -0800

    100com

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,34 +11,21 @@
  * If there are no numbers in the string, return 0.
  * Return: first integer found in string
  */
-
-int _atoi(char *s)
 int _atoi(char *s)
 {
-	int i;
-	int h, p;
+	int sign = 1;
+	unsigned int num = 0;
 
-	h = 0;
-	p = -1;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-			p *= -1;
+	do {
 
-		if (s[i] > 47 && s[i] < 58)
-		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
+		if (*s == '-')
+			sign *= -1;
 
-			if (s[i + 1] < 48  ||  s[i + 1] > 57)
-				break;
-		}
-	}
-	if (p < 0)
-		h *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-	return (h);
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
-
